88ec0c0b0572192114e54bd3bf2cb6495658861b
JWen00 <jenniferwen00@gmail.com>
2021-02-15 18:54:06 +1100

    Change from public dns to IPv4 address

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const app = express()
 const port = 80
+const host = '3.137.179.76'
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.listen(port, 'ec2-3-137-179-76.us-east-2.compute.amazonaws.com', () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+app.listen(port, host, () => {
+  console.log(`Example app listening at http://${host}:${port}`)
 })
